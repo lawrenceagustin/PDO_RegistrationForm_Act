f50f1adae89ffe4fb4027f879092dfba20c566b4
lawrenceagustin <167160419+lawrenceagustin@users.noreply.github.com>
2024-10-12 17:07:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,33 @@
 <?php require_once 'core/dbConfig.php';?>
-<?php require_once 'core/model.php'; ?>
+<?php require_once 'core/models.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        body{
-            font-family: Arial, Helvetica, sans-serif;
-        }
-        input{
-            font-size: 1.5cm;
-            height: 50px;
-            width: 200px;
-        }
-    </style>
+    <title>Pop-up Shop Registration Form</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <h3>Welcome to the Pop-up shop stall. Input your details here to register.</h3>
+    <h3>Welcome to the Pop-up shop stall registration. Input your details here to register.</h3>
     <form action="core/handleForms.php" method="POST">
-        <p><label for="firstName">First Name</label> <input type="text" name="firstName"></p>
-        <p><label for="lastName">Last Name</label> <input type="text" name="lastName"></p>
-        <p><label for="emailAddress">E-mail</label> <input type="text" name="emailAddress"></p>
-        <p><label for="shopName">Shop Name</label> <input type="text" name="shopName"></p>
-        <p><label for="contactNumber">Contact no.</label> <input type="text" name="contactNumber"></p>
-        <p><label for="stallNumber">Stall no.</label> <input type="text" name="stallNumber"></p>
-        <p><label for="eventDate">Event Date</label> <input type="text" name="eventDate"></p>
-        <p><label for="dateAdded">Date Added</label> <input type="text" name="dateAdded">
-            <input type="submit" name="insterNewShopBtn">
+        <p><label for="firstName">First Name:</label> <input type="text" name="firstName"></p>
+        <p><label for="lastName">Last Name:</label> <input type="text" name="lastName"></p>
+        <p><label for="emailAddress">E-mail:</label> <input type="text" name="emailAddress"></p>
+        <p><label for="shopName">Shop Name:</label> <input type="text" name="shopName"></p>
+        <p><label for="contactNumber">Contact no.:</label> <input type="text" name="contactNumber"></p>
+        <p><label for="stallNumber">Stall no.:</label> <input type="text" name="stallNumber"></p>
+        <p><label for="eventDate">Event Date:</label> <input type="date" name="eventDate" ></p>
+        <br><br>
+            <input type="submit" id="btn" name="insertNewShopBtn">
         </p>
     </form>
+    <a href="testGetVariable.php?lastName=Agustin&shopName=nekoHanekawa">Test Get Superglobal</a>
 
-    <table style="width:50%; margin-top: 50px;">
+    <table>
         <tr>
+            <th>ID</th>
             <th>First name</th>
             <th>Last name</th>
             <th>E-mail</th>
@@ -48,14 +41,19 @@
         <?php $seeAllStallRecords = seeAllStallRecords($pdo); ?>
         <?php foreach ($seeAllStallRecords as $row) { ?>
             <tr>
-                <td><?php echo $row['firstName']; ?></td>
-                <td><?php echo $row['lastName']; ?></td>
-                <td><?php echo $row['emailAddress']; ?></td>
-                <td><?php echo $row['shopName']; ?></td>
-                <td><?php echo $row['contactNumber']; ?></td>
-                <td><?php echo $row['stallNumber']; ?></td>
-                <td><?php echo $row['eventDate']; ?></td>
-                <td><?php echo $row['dateAdded']; ?></td>
+                <td><?php echo $row['UserID']; ?></td>
+                <td><?php echo $row['first_name']; ?></td>
+                <td><?php echo $row['last_name']; ?></td>
+                <td><?php echo $row['email']; ?></td>
+                <td><?php echo $row['shop_name']; ?></td>
+                <td><?php echo $row['contact_no']; ?></td>
+                <td><?php echo $row['stall_number']; ?></td>
+                <td><?php echo $row['event_date']; ?></td>
+                <td><?php echo $row['DateAdded']; ?></td>
+                <td>
+                    <a href="editStall.php?UserID=<?php echo $row['UserID'];?>">Edit</a>
+                    <a href="deleteStall.php?UserID=<?php echo $row['UserID'];?>">Delete</a>
+                </td>
             </tr
         <?php } ?>
     </table>
