9e90695e0471ae3773b09cab4b2c1a3759d98e78
lawrenceagustin <167160419+lawrenceagustin@users.noreply.github.com>
2024-10-12 13:33:13 +0800

    Update popUpShop.sql

--- popUpShop.sql
+++ popUpShop.sql
@@ -1,11 +1,11 @@
 CREATE TABLE popupShop (
     UserID INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    email VARCHAR(50),
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    email VARCHAR(30),
     shop_name VARCHAR(50),
-    contact_no VARCHAR(255),
-    stall_number VARCHAR(255),
-    event_date VARCHAR(255),
+    contact_no VARCHAR(20),
+    stall_number VARCHAR(20),
+    event_date VARCHAR(20),
     DateAdded TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
