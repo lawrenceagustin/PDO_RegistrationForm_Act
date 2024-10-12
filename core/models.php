9b1a03116fca492d56dc23b5d37e57d98af65e4b
lawrenceagustin <167160419+lawrenceagustin@users.noreply.github.com>
2024-10-12 17:09:15 +0800

    Update models.php

--- core/models.php
+++ core/models.php
@@ -3,15 +3,15 @@
 require_once 'dbConfig.php';
 
 function insertIntoStallRecords($pdo, $first_name, $last_name, $email, $shop_name, $contact_no,
-$stall_number, $event_date, $date_added){
+$stall_number, $event_date, $DateAdded){
 
-    $sql = "INSERT INTO popupShop (first_name, last_name, email, shop_name, contact_no, stall_number,
-    event_date, date_added) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO popupshop (first_name, last_name, email, shop_name, contact_no, stall_number,
+    event_date, DateAdded) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
     $stmt = $pdo->prepare($sql);
 
     $executeQuery = $stmt->execute([$first_name, $last_name, $email, $shop_name, $contact_no,
-    $stall_number, $event_date, $date_added]);
+    $stall_number, $event_date, $DateAdded]);
 
     if ($executeQuery){
         return true;
@@ -19,10 +19,51 @@ $stall_number, $event_date, $date_added){
 }
 
 function seeAllStallRecords($pdo){
-    $sql = "SELECT * FROM popupShop";
+    $sql = "SELECT * FROM popupshop";
     $stmt = $pdo->prepare($sql);
     $executeQuery = $stmt->execute();
     if ($executeQuery){
         return $stmt->fetchAll();
+    }
+}
+
+function getUserByID($pdo, $UserID){
+    $sql = "SELECT * FROM popupshop WHERE UserID = ?";
+    $stmt = $pdo->prepare($sql);
+    if($stmt->execute([$UserID])){
+        return $stmt->fetch();
+    }
+}
+
+function updateAStall($pdo, $first_name, $last_name, $email, $shop_name, $contact_no,
+$stall_number, $event_date, $DateAdded, $UserID){
+    $sql = "UPDATE popupshop
+                SET first_name = ?,
+                    last_name = ?,
+                    email = ?,
+                    shop_name = ?,
+                    contact_no = ?, 
+                    stall_number = ?,
+                    event_date = ?,
+                    DateAdded = ?
+            WHERE UserID = ?";
+    $stmt = $pdo->prepare($sql);
+
+    $executeQuery = $stmt->execute([$first_name, $last_name, $email, $shop_name, $contact_no,
+        $stall_number, $event_date, $DateAdded, $UserID]);
+    
+    if ($executeQuery){
+        return true;
+    }
+}
+
+function deleteAStall($pdo, $UserID){
+    $sql = "DELETE FROM popupshop WHERE UserID = ?";
+    $stmt = $pdo->prepare($sql);
+
+    $executeQuery = $stmt->execute([$UserID]);
+
+    if($executeQuery){
+        return true;
     }
 }
