58ca6295c01f9221a7f1fa5683e4a86d41b3f632
lawrenceagustin <167160419+lawrenceagustin@users.noreply.github.com>
2024-10-12 13:40:03 +0800

    Update dbConfig.php

--- core/dbConfig.php
+++ core/dbConfig.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $user = "root";
 $password = "";
-$dbname = "dave"; //db name
+$dbname = "popupshop"; //db name
 $dsn = "mysql:host={$host};dbname={$dbname}";
 
 $pdo = new PDO($dsn, $user, $password);
